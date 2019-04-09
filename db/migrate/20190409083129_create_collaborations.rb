ab9b989cb484c5bff465cd03f426d2bf31dee0df
carmen lok <carmen.lok08@gmail.com>
2019-04-09 20:30:32 +0800

    reset database

--- db/migrate/20190409083129_create_collaborations.rb
+++ db/migrate/20190409083129_create_collaborations.rb
@@ -3,8 +3,8 @@ class CreateCollaborations < ActiveRecord::Migration[5.2]
     create_table :collaborations do |t|
       t.datetime :completed_at
       t.string :image_url
-      t.references :drawings, foreign_key: true
-      t.references :creators, foreign_key: true
+      t.references :drawing, foreign_key: true
+      t.references :creator, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20190409110248_add_fk_constraint_to_drawings_table.rb
+++
@@ -1,5 +0,0 @@
-class AddFkConstraintToDrawingsTable < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :drawings, :kids, index: true
-  end
-end
