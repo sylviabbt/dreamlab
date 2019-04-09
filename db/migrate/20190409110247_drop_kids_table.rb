c2094daaa9a95c788b4206e49c93a14b8f8e8222
carmen lok <carmen.lok08@gmail.com>
2019-04-09 20:30:32 +0800

    reset database

--- db/migrate/20190409110247_drop_kids_table.rb
+++
@@ -1,6 +0,0 @@
-class DropKidsTable < ActiveRecord::Migration[5.2]
-  def change
-    remove_foreign_key  "drawings", "kids"
-    drop_table :kids
-  end
-end
--- db/migrate/20190409112743_add_drawing_to_collaborations.rb
+++
@@ -1,5 +0,0 @@
-class AddDrawingToCollaborations < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :collaborations, :drawing, foreign_key: true
-  end
-end
