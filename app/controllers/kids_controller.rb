d8d55014d1675c13dadfba9f4b2fdef568b904e8
carmen lok <carmen.lok08@gmail.com>
2019-04-10 14:17:48 +0800

    kids controller, views, sign-up, policy

---
+++ app/controllers/kids_controller.rb
@@ -0,0 +1,48 @@
+class KidsController < ApplicationController
+     before_action :set_kid, only: [:show, :edit, :update]
+
+    def show
+     #refer to private method set_kid, called through before_action ()top line)
+    end
+
+    def new
+      @kid = Kid.new
+      authorize @kid
+    end
+
+    def create
+      @kid = Kid.new(kid_params)
+      # ERROR user is not a method? isn't it a method from App policy?
+      # @kid.user = current_user
+      authorize @kid
+      #the correct fields were entered and saved
+      if @kid.save
+        redirect_to kid_path(@kid)
+      else
+        render :new
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @kid.update(kid_params)
+        redirect_to kid_path(@kid)
+      else
+        render :edit
+      end
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_kid
+      @kid = Kid.find(params[:id])
+      authorize @kid
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def kid_params
+      params.require(:kid).permit(:email, :password, :first_name, :age, :avatar, :city, :description, :favourite_things, :dislikes)
+    end
+end
