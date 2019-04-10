16cd170a2bce5ab7590390b6ddcfc3f9f8a00299
Sylvia Bouboutou <sylviabouboutou@hotmail.com>
2019-04-10 15:37:27 +0800

    index and show ok

--- app/policies/drawing_policy.rb
+++ app/policies/drawing_policy.rb
@@ -5,19 +5,19 @@ class DrawingPolicy < ApplicationPolicy
     end
   end
 
-  def create
+  def create?
     true
   end
 
-  def new
+  def new?
     true
   end
 
-  def index
+  def index?
     true
   end
 
-  def show
+  def show?
     true
   end
 end
