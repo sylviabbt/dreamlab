68d0f1385b9a37e39b7022d0bce7c0d5a923d82a
Sylvia Bouboutou <sylviabouboutou@hotmail.com>
2019-04-11 18:17:45 +0800

    removed fk constraint that is not neccessary

--- db/migrate/20190411073659_remove_fk_constraint_drawing_kids.rb
+++
@@ -1,6 +0,0 @@
-class RemoveFkConstraintDrawingKids < ActiveRecord::Migration[5.2]
-  def change
-    remove_foreign_key(:drawings, :kids)
-    add_foreign_key(:drawings, :users, column: :kid_id)
-  end
-end
