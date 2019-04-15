6622070f21770322d831e963aadb247af6e7841b
carmen lok <carmen.lok08@gmail.com>
2019-04-15 10:07:30 +0800

    re-routing the collabs paths

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
 
   def home
+        @collaborations = policy_scope(Collaboration).order(created_at: :desc)
+
   end
 end
