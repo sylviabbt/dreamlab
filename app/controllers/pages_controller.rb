245e0f31f4d688631ef280ee0d8216d089b34b08
carmen lok <carmen.lok08@gmail.com>
2019-04-15 10:32:09 +0800

    removed the book drawing button from drawing show, if user is a kid

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
 
   def home
-        @collaborations = policy_scope(Collaboration).order(created_at: :desc)
-
+    @collaborations = policy_scope(Collaboration).order(created_at: :desc)
   end
 end
