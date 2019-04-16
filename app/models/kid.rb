a0b89e57dda01ff625941c38a2bfa1dc7595718e
carmen lok <carmen.lok08@gmail.com>
2019-04-16 16:03:40 +0800

    tags on the child's profile for fav things and dislikes

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -3,4 +3,7 @@ class Kid < User
   has_many :collaborations, through: :drawings
 
   mount_uploader :avatar, PhotoUploader
+
+   acts_as_taggable
+   acts_as_taggable_on :favourite_things, :dislikes
 end
