71a733703fcff33405232198e6a2e60c2c8a8aed
carmen lok <carmen.lok08@gmail.com>
2019-04-16 19:08:59 +0800

    renaming the dislikes column in user to worst_things

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_071106) do
+ActiveRecord::Schema.define(version: 2019_04_16_105311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,7 +88,7 @@ ActiveRecord::Schema.define(version: 2019_04_16_071106) do
     t.string "last_name"
     t.string "portfolio_url"
     t.string "favourite_things"
-    t.string "dislikes"
+    t.string "worst_things"
     t.string "type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
