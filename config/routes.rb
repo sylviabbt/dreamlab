84aba0b619bf99fa9710f3c501408a5965f285d9
carmen lok <carmen.lok08@gmail.com>
2019-05-07 17:25:17 +0800

    folder structure for WX

--- config/routes.rb
+++ config/routes.rb
@@ -26,3 +26,36 @@ Rails.application.routes.draw do
   end
 
 end
+
+
+# =========================================
+# Routes wx-mini-programme
+Rails.application.routes.draw do
+  # [...]
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+
+      resources :kids, only: [:create, :new, :show, :edit, :update] do
+      resources :drawings, only: [:create, :new, :show]
+      resources :collaborations, only: [:show]
+    end
+
+    resources :drawings, only: [:index, :destroy] do
+      get '/download' => 'drawings#download'
+    end
+
+    resources :creators, only: [:index, :create, :new, :show, :edit, :update] do
+      resources :collaborations, only: [:show, :edit, :update, :destroy]
+    end
+
+    resources :posts, only: [:index, :show, :create, :new, :edit, :update, :destroy]
+
+    resources :collaborations, only: [:index, :create,] do
+      # new path =>  /collaborations/23/upvote
+      get '/download' => 'collaborations#download'
+      get '/upvote' => 'collaborations#upvote'
+    end
+
+    end
+  end
+end
