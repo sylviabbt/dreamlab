f9d3f0edc9a0282d96c03d1c5680c1b3e7061e81
Sylvia Bouboutou <sylviabouboutou@hotmail.com>
2019-05-14 16:25:05 +0800

    mailer code deactivated

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,18 +1,18 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
-class UserMailerPreview < ActionMailer::Preview
+# class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
-  # def welcome
-  #   UserMailer.welcome
-  # end
+# Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
+# def welcome
+#   UserMailer.welcome
+# end
 
-  def booked
-    kid = Kid.first
-    UserMailer.booked(kid)
-  end
+#   def booked
+#     kid = Kid.first
+#     UserMailer.booked(kid)
+#   end
 
-  def completed
-    kid = Kid.first
-    UserMailer.completed(kid)
-  end
-end
+#   def completed
+#     kid = Kid.first
+#     UserMailer.completed(kid)
+#   end
+# end
