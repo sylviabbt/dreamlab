18865949fff825bab96408893df3fa9f001181c4
Ivy Yu <ivys.yu@gmail.com>
2019-06-05 16:23:22 +0800

    created simple about us page with partners

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get "/dream-gallery" => "pages#dream_gallery"
+  get "/about-us" => "pages#about_us"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :kids, only: [:create, :new, :show, :edit, :update] do
