5e6ff11de5ce1da989223bd562fdbcbdf3109e3c
Nini Zhu <pn.zhu76@gmail.com>
2019-06-08 11:57:25 +0800

    updated creator's sign up page, first & last name mandatory

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  # before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: [:home, :dream_gallery]
   include Pundit
@@ -12,6 +13,9 @@ class ApplicationController < ActionController::Base
 
 
   private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :city, :age])
+  end
 
   def after_sign_in_path_for(user)
     if user.type == "Kid"
@@ -28,4 +32,5 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+
 end
